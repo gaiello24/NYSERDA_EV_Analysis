bde530880ad9485815e1a46f6fff1249b646a86d
gaiello24 <gaiello24@gmail.com>
2021-08-27 12:07:37 -0400

    WIP

--- EV_Analysis_Script.R
+++ EV_Analysis_Script.R
@@ -10,15 +10,21 @@ names(ev_data_list) <- sheet_names
 
 #Begin work with Charging Use data
 
-#Load tidyverse
+#Load packages
 
 library(tidyverse)
+library(lubridate)
+
+#mutate data to include Year Month and Day columns
 
 charging_data <- ev_data_list$`Charging Use`
 names(charging_data) <- names(charging_data) %>%
   str_replace_all(c("[(]" = "", "[)]" = "", " " = "_"))
   
 #Summarize total energy usage by zip code
+
+charging_data <- charging_data %>% 
+  mutate(Start_Year = year(Start_Date), Start_Month = month(Start_Date))
 
 charging_data %>%
   group_by(ZIP_Code) %>%
